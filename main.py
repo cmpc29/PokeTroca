49f8c43056d6c49615ac22532a532dde6e295309
Caio Merçon <cmpc29@hotmail.com.br>
2025-01-15 12:16:58 -0300

    Config inicial do BD

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 import sqlite3
+
+connect = sqlite3.connect("Pokedex.db")
+cursor = connect.cursor()
---
+++ sqlite.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE dex (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     pokeEspecie TEXT NOT NULL
+-- );
+
+-- ALTER TABLE dex ADD COLUMN regiao TEXT NOT NULL;
+-- ALTER TABlE dex DROP COLUMN regiao;
+-- INSERT INTO dex (pokeEspecie,regiao)
+-- VALUES ('pidgey','Kanto');
+-- SELECT * FROM dex;
+
