5f090521b292364f985778c1b7c4a5bdc393216c
Caio Merçon <cmpc29@hotmail.com.br>
2025-01-15 12:58:04 -0300

    Formata o banco de dados

--- sqlite.sql
+++ sqlite.sql
@@ -3,9 +3,15 @@
 --     pokeEspecie TEXT NOT NULL
 -- );
 
--- ALTER TABLE dex ADD COLUMN regiao TEXT NOT NULL;
+-- ALTER TABLE dex ADD COLUMN tipo1 TEXT NOT NULL;
+-- ALTER TABLE dex ADD COLUMN tipo2 TEXT;
+-- ALTER TABLE dex ADD COLUMN nivel INTEGER NOT NULL;
+-- ALTER TABLE dex ADD COLUMN shiny BOOLEAN NOT NULL;
+
+ 
 -- ALTER TABlE dex DROP COLUMN regiao;
--- INSERT INTO dex (pokeEspecie,regiao)
--- VALUES ('pidgey','Kanto');
+-- INSERT INTO dex (pokeEspecie,tipo1,nivel,shiny)
+-- VALUES ('pidgey','voador',12,'True');
+-- DELETE FROM dex WHERE pokeEspecie='pidgey';
 -- SELECT * FROM dex;
 
